ec3e41b699d3215a658850d55b9ce1f6bd75f1d1
Andrey <glinkin.a.o@yandex.ru>
2020-12-06 20:02:09 +1000

    30% Color

--- homeWork2.2 UIKitApp/ViewController.swift
+++ homeWork2.2 UIKitApp/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var colorView: UIView!
+   
+    @IBOutlet var redSlider: UISlider! {
+        didSet {
+            redSlider.tintColor = UIColor.red
+        }
+}
+    @IBOutlet var greenSlider: UISlider! {
+        didSet {
+            greenSlider.tintColor = UIColor.green
+        }
+}
+    @IBOutlet var blueSlider: UISlider! {
+        didSet {
+            blueSlider.tintColor = UIColor.blue
+        }
+}
+    
+    @IBOutlet var statusLabelRed: UILabel!
+    @IBOutlet var statusLabelGreen: UILabel!
+    @IBOutlet var statusLabelBlue: UILabel!
+    @IBOutlet var nameLabelRed: UILabel!
+    @IBOutlet var nameLabelGreen: UILabel!
+    @IBOutlet var nameLabelBlue: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
     }
 
-
 }
-
